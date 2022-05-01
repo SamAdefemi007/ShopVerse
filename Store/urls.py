e2dc25a6ba0a32184b1a0cf23edbd22ac2637098
SamAdefemi007 <91951022+SamAdefemi007@users.noreply.github.com>
2022-05-01 11:19:00 +0100

    urls configuration

--- Store/urls.py
+++ Store/urls.py
@@ -1,6 +1,17 @@
 from django.urls import path
 from . import views
 
+
 urlpatterns = [
-    path('', views.home, name="home"),
+    path('home/', views.homepage, name="homepage"),
+    path('products', views.products, name="products"),
+    path('products/<int:product_id>', views.productDetail, name="productDetail"),
+    path('register/', views.register, name="register"),
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('carts/', views.cart, name="cart"),
+    path('checkout/', views.checkout, name="checkout"),
+    path('add_cart/<int:product_id>', views.add_to_cart, name="add_to_cart"),
+    path('remove_cart/<int:product_id>',
+         views.remove_from_cart, name="remove_from_cart"),
+
 ]

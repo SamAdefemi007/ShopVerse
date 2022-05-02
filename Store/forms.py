d93bbec94a9e2618bcbb46280d64cbb89eb6a751
SamAdefemi007 <91951022+SamAdefemi007@users.noreply.github.com>
2022-05-02 14:42:02 +0100

    styled the password reset page

--- Store/forms.py
+++ Store/forms.py
@@ -8,16 +8,25 @@ from django.contrib.auth.forms import AuthenticationForm
 class RegisterForm(UserCreationForm):
     username = forms.CharField(max_length=200)
     first_name = forms.CharField(max_length=30)
+    password1 = forms.CharField(
+        label=("Password"),
+        strip=False,
+        widget=forms.PasswordInput,
+    )
+    password2 = forms.CharField(
+        label=("Confirm Password"),
+        strip=False,
+        widget=forms.PasswordInput,
+    )
     last_name = forms.CharField(max_length=30)
     email = forms.EmailField(max_length=50)
-    phone_number = forms.CharField()
-    birth_date = forms.DateField(initial=datetime.date.today)
+    phone_number = forms.CharField(required=False)
+    birth_date = forms.DateField(widget=forms.SelectDateWidget(
+        empty_label=("Choose Year", "Choose Month", "Choose Day"),
+    ),
+        label=("Date of birth"), initial=datetime.date.today, required=False)
 
     class Meta:
         model = User
         fields = ('username', 'first_name', 'last_name', 'email', 'password1', 'password2',
                   'birth_date', 'phone_number')
-
-    def __init__(self, *args, **kwargs):
-        super(RegisterForm, self).__init__(*args, **kwargs)
-        self.fields['username'].widget.attrs.update({'class': 'login-style'})
